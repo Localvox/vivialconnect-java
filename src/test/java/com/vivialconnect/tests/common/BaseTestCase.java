6ae017dbb9c9b9c853bc4c2997ebb838d1d4519c
Carlos Gonzalez <c.gonzalez@jellyhub.com>
2017-07-17 20:03:22 -0400

    Added first test cases

--- src/test/java/com/vivialconnect/tests/common/BaseTestCase.java
+++ src/test/java/com/vivialconnect/tests/common/BaseTestCase.java
@@ -1,11 +1,16 @@
 package com.vivialconnect.tests.common;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vivialconnect.client.VivialConnectClient;
 
-public class BaseTestCase
-{
+public class BaseTestCase{
 	
 	private static final int INVALID_ACCOUNT_ID = 0;
 	private static final String INVALID_API_KEY = "";
@@ -17,25 +22,54 @@ public class BaseTestCase
 	
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void client_initialization_with_invalid_account_id_throws_illegal_argument_exception(){
+	public static void client_initialization_with_invalid_account_id_throws_illegal_argument_exception(){
 		VivialConnectClient.init(INVALID_ACCOUNT_ID, API_KEY, API_SECRET);
 	}
 	
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void client_initialization_with_invalid_api_key_throws_illegal_argument_exception(){
+	public static void client_initialization_with_invalid_api_key_throws_illegal_argument_exception(){
 		VivialConnectClient.init(ACCOUNT_ID, INVALID_API_KEY, API_SECRET);
 	}
 	
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void client_initialization_with_invalid_api_secret_throws_illegal_argument_exception(){
+	public static void client_initialization_with_invalid_api_secret_throws_illegal_argument_exception(){
 		VivialConnectClient.init(ACCOUNT_ID, API_KEY, INVALID_API_SECRET);
 	}
 	
 	
-	protected String loadFixture(String filename)
-	{
+	protected <T> T loadFixture(String filename, Class<T> type){
+		return loadFixture(filename, type, true);
+	}
+	
+	
+	protected <T> T loadFixture(String filename, Class<T> type, boolean unwrapRoot){
+		ClassLoader classLoader = BaseTestCase.class.getClassLoader();
+		InputStream stream = classLoader.getResourceAsStream(String.format("%s.json", filename));
+		
+		try{
+			String content = IOUtils.toString(stream);
+			
+			ObjectMapper mapper = getObjectMapper();
+			if (unwrapRoot){
+	            mapper.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
+	        }
+			
+			return mapper.reader().forType(type).readValue(content);
+		}
+		catch (IOException ioe){
+			System.err.println(String.format("Failed to load fixture ('%s'): %s", filename, ioe));
+		}
+		
 		return null;
+	}
+
+
+	private ObjectMapper getObjectMapper(){
+		ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        
+		return mapper;
 	}
 }
--- src/test/java/com/vivialconnect/tests/common/account/AccountTest.java
+++ src/test/java/com/vivialconnect/tests/common/account/AccountTest.java
@@ -1,7 +1,66 @@
 package com.vivialconnect.tests.common.account;
 
-public class AccountTest
-{
+import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.vivialconnect.client.VivialConnectClient;
+import com.vivialconnect.model.account.Account;
+import com.vivialconnect.model.error.VivialConnectException;
+import com.vivialconnect.tests.common.BaseTestCase;
+
+public class AccountTest extends BaseTestCase{
 	
+	private Account testAccount;
+	
+	@BeforeClass
+	public static void initializeClient(){
+		VivialConnectClient.init(ACCOUNT_ID, API_KEY, API_SECRET);
+	}
+	
+	
+	@Before
+	public void loadAccount(){
+		if (testAccount == null){
+			testAccount = loadFixture("account", Account.class);
+		}
+	}
+	
+	
+	@Test
+	public void test_get_account() throws VivialConnectException{
+		Account account = Account.getAccount();
+		
+		assertEquals(testAccount.getId(), account.getId());
+	}
+	
+	
+	@Test
+	public void test_update_account() throws VivialConnectException{
+		Account account = Account.getAccount();
+		Date modifiedDateBeforeUpdate = account.getDateModified();
+		
+		String newCompanyName = getNewCompanyName(account);
+		account.setCompanyName(newCompanyName);
+		account.update();
+		
+		assertEquals(newCompanyName, account.getCompanyName());
+		assertNotEquals(modifiedDateBeforeUpdate.getTime(), account.getDateModified().getTime());
+	}
+
+
+	private String getNewCompanyName(Account account){
+		String testAccountCompanyName = testAccount.getCompanyName();
+		String newCompanyName = testAccountCompanyName;
+		
+		if (testAccountCompanyName.equals(account.getCompanyName())){
+			newCompanyName = "Vivial Connect";
+		}
+		
+		return newCompanyName;
+	}
 }
