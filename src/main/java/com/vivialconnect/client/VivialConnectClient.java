52c9fb30de3f45d8726fd0afd1527017c165f3d0
chuckbuckethead <cgonzalez@cgonzalez.nt.core>
2017-07-18 12:25:38 -0400

    Fixed formatting in VivialConnectClient class

--- src/main/java/com/vivialconnect/client/VivialConnectClient.java
+++ src/main/java/com/vivialconnect/client/VivialConnectClient.java
@@ -8,29 +8,24 @@ public final class VivialConnectClient {
     private static String apiSecret;
 
     public static final String API_BASE = "https://api.vivialconnect.net/api/v1.0";
-    
-    
-    private VivialConnectClient(){
+
+    private VivialConnectClient() {
 
     }
 
-
-    public static String getApiKey(){
+    public static String getApiKey() {
         return apiKey;
     }
 
-
-    public static String getApiSecret(){
+    public static String getApiSecret() {
         return apiSecret;
     }
 
-
-    public static int getAccountId(){
+    public static int getAccountId() {
         return accountId;
     }
 
-
-    public static void init(int accountId, String apiKey, String apiSecret){
+    public static void init(int accountId, String apiKey, String apiSecret) {
         validateInitialArguments(accountId, apiKey, apiSecret);
 
         VivialConnectClient.accountId = accountId;
@@ -38,40 +33,33 @@ public final class VivialConnectClient {
         VivialConnectClient.apiSecret = apiSecret;
     }
 
+    private static void validateInitialArguments(int accountId, String apiKey, String apiSecret) {
+        validateAccountId(accountId);
+        validateAPIKey(apiKey);
+        validateAPISecret(apiSecret);
+    }
 
-    private static void validateInitialArguments(int accountId, String apiKey, String apiSecret){
-    	validateAccountId(accountId);
-    	validateAPIKey(apiKey);
-    	validateAPISecret(apiSecret);
-	}
+    private static void validateAccountId(int accountId) {
+        if (accountId < 1) {
+            throw createIllegalArgumentException("accountId");
+        }
+    }
 
+    private static void validateAPIKey(String apiKey) {
+        validateStringArg(apiKey, "apiKey");
+    }
 
-	private static void validateAccountId(int accountId){
-		if (accountId < 1){
-			throw createIllegalArgumentException("accountId");
-		}
-	}
+    private static void validateAPISecret(String apiSecret) {
+        validateStringArg(apiSecret, "apiSecret");
+    }
 
+    private static void validateStringArg(String arg, String argName) {
+        if (arg == null || arg.isEmpty()) {
+            throw createIllegalArgumentException(argName);
+        }
+    }
 
-	private static void validateAPIKey(String apiKey){
-		validateStringArg(apiKey, "apiKey");
-	}
-
-
-	private static void validateAPISecret(String apiSecret){
-		validateStringArg(apiSecret, "apiSecret");
-	}
-	
-	
-	private static void validateStringArg(String arg, String argName){
-		if (arg == null || arg.isEmpty())
-		{
-			throw createIllegalArgumentException(argName);
-		}
-	}
-	
-	
-	private static IllegalArgumentException createIllegalArgumentException(String argName){
-		return new IllegalArgumentException(String.format("'%s' param is not valid", argName));
-	}
+    private static IllegalArgumentException createIllegalArgumentException(String argName) {
+        return new IllegalArgumentException(String.format("'%s' param is not valid", argName));
+    }
 }
