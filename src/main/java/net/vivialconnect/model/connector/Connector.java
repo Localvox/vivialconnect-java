9340ac3575dc5ba9fb0505e6b499317fbb5a9346
Carlos Gonzalez <c.gonzalez@jellyhub.com>
2017-07-23 18:33:27 -0400

    Removed unused import from Connector class

--- src/main/java/net/vivialconnect/model/connector/Connector.java
+++ src/main/java/net/vivialconnect/model/connector/Connector.java
@@ -3,11 +3,12 @@ package net.vivialconnect.model.connector;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
+
 import net.vivialconnect.model.ResourceCount;
 import net.vivialconnect.model.VivialConnectResource;
-import net.vivialconnect.model.account.Contact;
 import net.vivialconnect.model.error.NoContentException;
 import net.vivialconnect.model.error.VivialConnectException;
 import net.vivialconnect.model.format.JsonBodyBuilder;
