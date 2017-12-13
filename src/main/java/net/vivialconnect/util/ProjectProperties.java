7dd7417507a1e8e7f3ad53d74a176609c0ab023c
David <dpitchford@vivial.net>
2017-12-13 14:14:48 -0600

    Removed dependency on Spring to get app properties.

--- src/main/java/net/vivialconnect/util/ProjectProperties.java
+++ src/main/java/net/vivialconnect/util/ProjectProperties.java
@@ -4,19 +4,17 @@ import java.util.Properties;
 import java.io.InputStream;
 import java.io.IOException;
 
-import org.springframework.core.io.ClassPathResource;
-
 public class ProjectProperties {
 
     private static Properties p = null;
 
     public static void getProperties() throws IOException {
 
-        ClassPathResource resource = new ClassPathResource("app.properties");
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         p = new Properties();
         InputStream inputStream = null;
         try {
-            inputStream = resource.getInputStream();
+            inputStream = classLoader.getResourceAsStream("app.properties");
             p.load(inputStream);
         } catch (IOException e) {
             System.out.println(e.getMessage());
